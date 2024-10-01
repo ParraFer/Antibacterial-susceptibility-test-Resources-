a20e2f216e7bb6be6ac4698034013cc596e8733e
Parra_Fernando <71463559+ParraFer@users.noreply.github.com>
2024-10-01 16:53:32 -0600

    Update README.cmd

--- Dataset/README.cmd
+++ Dataset/README.cmd
@@ -1,1 +1,1 @@
-URL Test part: https://drive.google.com/file/d/1bwSC5zB7TK5j7P0t28H7Tmmv6jRyXqBa/view?usp=drive_link
+In this folder, you will find the dataset intended for the training and testing of the neural network. The train_data file contains a total of 74 images, while the test_data file includes 78 images. Additionally, the train_dataAugmentation file represents the augmented training data, resulting in a total of 219 images available for the neural network training process.
